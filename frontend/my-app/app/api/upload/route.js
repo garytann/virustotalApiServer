dfaf94769a8cc13b04e5f3a298ca8e3ac95c8377
garytann <garytannjy@gmail.com>
2024-01-15 12:17:57 +0800

    fixed the route to receive data from client component and update the data table

--- frontend/my-app/app/api/upload/route.js
+++ frontend/my-app/app/api/upload/route.js
@@ -1,32 +1,9 @@
 
 import { NextResponse } from 'next/server';
-// export async function POST() {
-//   const res = await fetch(
-//     "http://localhost:8000/upload"
-//   );
-//   const data = await res.json();
-//   return NextResponse.json(data);
-// }
-// import {NextResponse} from "next/server";
-// import fs from 'fs';
-// import { pipeline } from 'stream';
-// import { promisify } from 'util';
-// const pump = promisify(pipeline);
 
-// export async function POST(req,res) {
-//     try{
-//         const formData = await req.formData();
-//         const file = formData.getAll('files')[0]
-//         // const filePath = `./public/file/${file.name}`;
-//         // await pump(file.stream(), fs.createWriteStream(filePath));
-//         return NextResponse.json({status:"success",data:file.size})
-//     }
-//     catch (e) {
-//         return  NextResponse.json({status:"fail",data:e})
-//     }
-// }
 export async function POST(req) {
     // const formData = new FormData();
+    // ipmport to use this to set the file data that receive from client
     const formData = await req.formData();
     // const fileInput = req.files.file; // Assuming the file input field name is "file"
 
@@ -41,46 +18,3 @@ export async function POST(req) {
     console.log("this is server res data", data);
     return NextResponse.json(data);
 }
-
-// import formidable from 'formidable';
-// import fs from 'fs';
-
-// export const config = {
-//   api: {
-//     bodyParser: false, // Disable built-in bodyParser to handle it manually with formidable
-//   },
-// };
-
-// export default function handler(req, res) {
-//   if (req.method === 'POST') {
-//     const form = new formidable.IncomingForm();
-
-//     form.parse(req, (err, fields, files) => {
-//       if (err) {
-//         console.error('Error parsing form:', err);
-//         res.status(500).json({ error: 'Error parsing form data' });
-//         return;
-//       }
-
-//       // Here, you can handle the uploaded file in the "files" object
-//       // For example, move the file to a desired location on the server
-//       // fs.renameSync(files.file.path, '/path/to/desired/location/' + files.file.name);
-
-//       res.status(200).json({ message: 'File uploaded successfully' });
-//     });
-//   } else {
-//     res.status(405).json({ error: 'Method not allowed' });
-//   }
-// }
-
-// export async function POST(req){
-//     const formData = await req.formData();
-//     // const file = formData.getAll('files')[0]
-//     const file = formData.get('files')
-//     // const filePath = `./public/file/${file.name}`;
-//     // await pump(file.stream(), fs.createWriteStream(filePath));
-//     // return NextResponse.json({status:"success",data:file.size})
-//     const bytes = await file.arrayBuffer();
-//     const buffer = Buffer.from(bytes);
-//     return NextResponse.json({status:"success"})
-// }
--- frontend/my-app/components/Table.js
+++ frontend/my-app/components/Table.js
@@ -1,55 +1,88 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Space, Table, Tag } from 'antd';
 const { Column, ColumnGroup } = Table;
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, message, Upload } from 'antd';
 
-const props = {
-    name: 'file',
-    action: 'http://localhost:3000/api/upload',
-    headers: {
-        authorization: 'authorization-text',
-    },
-    async onChange(info) {
-        if (info.file.status !== 'uploading') {
-            console.log(info.file, info.fileList);
-        }
-        if (info.file.status === 'done') {
-            message.success(`file uploaded successfully`);
-        } else if (info.file.status === 'error') {
-            message.error(`file upload failed.`);
-        }
-    },
-};
-
 
 export default function DataTable() {
+
+    const [dataSource, setDataSource] = React.useState([]);
+    
+    const props = {
+        name: 'file',
+        action: 'http://localhost:3000/api/upload',
+        headers: {
+            authorization: 'authorization-text',
+        },
+        async onChange(info) {
+            if (info.file.status !== 'uploading') {
+                console.log(info.file, info.fileList);
+                console.log("hello world");
+            }
+            if (info.file.status === 'done') {
+                message.success(`file uploaded successfully`);
+                // console.log('##info', info.file.response.data);
+                setDataSource(info.file.response.data);
+
+            } else if (info.file.status === 'error') {
+                message.error(`file upload failed.`);
+            }
+        },
+    };
+
+    const [column] = React.useState([
+        {
+            title: 'ID',
+            dataIndex: 'analysis_id',
+            key: 'analysis_id',
+        },
+        {
+            title: 'File',
+            dataIndex: 'filename',
+            key: 'filename',
+        },
+        {
+            title: 'SHA256',
+            dataIndex: 'hash_id',
+            key: 'hash_id',
+        },
+        {
+            title: 'Detected',
+            dataIndex: 'type',
+            key: 'type',
+        },
+        {
+            title: 'Date',
+            dataIndex: 'date',
+            key: 'date',
+        }
+    ]);
+
     const [file, setFile] = React.useState(null);
 
-    // const handleFileChange = (e) => {
-    //     setFile(e.target.files[0]);
-    // };
+    const handleFileChange = (e) => {
+        console.log('hello world')
+        setFile(e.target.files[0]);
+    };
+
 
     // handle the file upload
     const uploadFile = async () => {
-        // e.preventDefault();
         try {
             const formData = new FormData();
-            console.log("this is file", file)
-            // formData.set('file', file);
             formData.append('file', file);
-            // formData.append('file', file);
             const res = await fetch('/api/upload', {
                 method: 'POST',
-                // headers: {
-                //     'Content-Type':'Multipart/form-data'
-                //   },
                 body: formData,
-            });
-            // const data = await res.json();
-            console.log(res.text());
+            }).then((res) => { console.log("data", res) });
+            const response = await res.json();
+
+            // console.log("helloworld");
+            // console.log('##response', response);
+
         } catch (error) {
             console.error('Error uploading:', error);
         }
@@ -57,22 +90,23 @@ export default function DataTable() {
 
     return (
         <div>
-            {/* <Upload onSubmit = {handleFileChange} {...props}> */}
-            <Upload {...props}>
-            {/* <Upload > */}
-                <Button onSubmit={uploadFile} icon={<UploadOutlined />}>Click to Upload</Button>
+            {/* <Upload onSubmit={handleFileChange} {...props}> */}
+            <Upload {...props} showUploadList={false}>
+                {/* <Upload > */}
+                {/* <Upload {...props}> */}
+                {/* <Upload > */}
+                <Button icon={<UploadOutlined />}>Click to Upload</Button>
             </Upload>
 
-            <Table dataSource={''}>
-                <Column title="ID" dataIndex="id" key="id" />
-                <Column title="File" dataIndex="fileName" key="fileName" />
-                <Column title="SHA256" dataIndex="fileHash" key="fileHash" />
-                <Column title="Detected" dataIndex="detected" key="detected" />
-                <Column title="Date" dataIndex="date" key="date" />
+            <Table dataSource={dataSource} columns={column}>
+                {/* <Column title="ID" dataIndex="analysis_id" key="analysis_id" />
+                <Column title="File" dataIndex="filename" key="filename" />
+                <Column title="SHA256" dataIndex="hash_id" key="hash_id" />
+                <Column title="Detected" dataIndex="type" key="type" />
+                <Column title="Date" dataIndex="date" key="date" /> */}
             </Table>
 
         </div>
     );
 
 };
-// export default DataTable;
