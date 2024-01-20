bdef4549feb91494a1b092ebd98cc4fc61fd96da
garytann <garytannjy@gmail.com>
2024-01-20 17:01:08 +0800

    fixed route handlers -> route handlers in containers connects to the server side through service name instead of host ip

--- frontend/my-app/app/api/upload/route.js
+++ frontend/my-app/app/api/upload/route.js
@@ -3,9 +3,14 @@ import { NextResponse } from 'next/server';
 
 
 export async function POST(req) {
+    // this will assign the neccesary env variable depending on the environment
+    // when is it compose in a docker container, the server side will connect to the backend container instead of IP
+    const apiUrl = process.env.NODE_ENV === 'production' ? 'backend' : 'localhost'
+
     const formData = await req.formData();
 
-    const res = await fetch('http://localhost:8000/upload', {
+    const res = await fetch(`http://${apiUrl}:8000/upload`, {
+    // const res = await fetch(`http://localhost:8000/upload`, {
         method: 'POST',
         body: formData,
     });
