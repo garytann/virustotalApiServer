391d246bc43f12a460fe6af375ee5d2886b2a6c6
garytann <garytannjy@gmail.com>
2024-01-30 15:20:59 +0800

    added hero section

--- frontend/my-app/components/HeroSection.js
+++ frontend/my-app/components/HeroSection.js
@@ -2,6 +2,9 @@
 'use client';
 import Container from './Container';
 import Link from 'next/link';
+import heroImg from "@/public/hero.png";
+import Image from "next/image"
+
 
 
 const HeroSection = () => {
@@ -19,12 +22,24 @@ const HeroSection = () => {
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
 
-            <Link href = "/dashboard"
+              <Link href="/dashboard"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
                 Get Started For Free
               </Link>
-              
             </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-center w-full lg:w-1/2">
+          <div className="">
+            <Image
+              src={heroImg}
+              width="616"
+              height="617"
+              className={"object-cover"}
+              alt="Hero Illustration"
+              loading="eager"
+              placeholder="blur"
+            />
           </div>
         </div>
       </Container>
